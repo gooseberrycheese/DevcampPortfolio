25368a212a48ce796f0df1bf3977cc98ab8e06c3
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:45:10 +0000

    fixed console blog creator

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,7 +28,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
       end
@@ -52,25 +52,25 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully deleted.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
   end
-  
+
   def toggle_status
-    
-      if @blog.draft?
-        @blog.published!
-        elsif @blog.published?
-        @blog.draft!
-      end  
-     redirect_to blogs_url, notice: 'Post status has been updated.'
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-     @blog = Blog.friendly.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,5 +5,5 @@ class Blog < ApplicationRecord
   
   validates_presence_of :title, :body
   
-  belongs_to :topi
+  belongs_to :topic
 end
