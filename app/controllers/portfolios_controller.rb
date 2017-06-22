1d2399e8076a3f0c2ed81d5b16ab34595600d5c9
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 16:45:32 +0000

    edited  portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,17 +2,18 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -22,28 +23,28 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-    
-    def edit
-      @portfolio_item = Portfolio.find(params[:id])
-    end
-    
-      def update
-              @portfolio_item = Portfolio.find(params[:id])
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
-      end
-      end
-      
-    def show
-      @portfolio_item = Portfolio.find(params[:id])
     end
-    
-  def destroy
+  end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
 
@@ -52,7 +53,8 @@ class PortfoliosController < ApplicationController
 
     # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Portfolio was removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
+
 end
