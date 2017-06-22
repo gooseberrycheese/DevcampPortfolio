3204e35c2c2cb92e0ba1c68d0171e58678308cc8
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:50:17 +0000

    implemented image defaults for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,17 +11,17 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
   
-    def create
+  def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
-    end
+  end
     
     def edit
       @portfolio_item = Portfolio.find(params[:id])
@@ -35,7 +35,7 @@ class PortfoliosController < ApplicationController
       else
         format.html { render :edit }
       end
-     end
+      end
       end
       
     def show
