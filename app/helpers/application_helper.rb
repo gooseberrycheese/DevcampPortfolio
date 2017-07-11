b78725bf5faf2260e993708b3e9b910014af364c
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 17:38:44 +0000

    updated links with edit profile and updated styles for user update page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,9 @@ module ApplicationHelper
        " ".html_safe +
        (link_to "Login", new_user_session_path, class: style) 
      else 
-       link_to "Logout", destroy_user_session_path, method: :delete, class: style
+       (link_to "Logout", destroy_user_session_path, method: :delete, class: style) +
+       " ".html_safe +
+       (link_to "Edit Profile", edit_user_registration_path, class: style)
       end 
   end
   
