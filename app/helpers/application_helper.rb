516928fe9aa846312a82dcb70ee5220ab0a5e29a
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 04:23:17 +0000

    implemented blog layout stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,23 @@
 module ApplicationHelper
- 
-  def login_helper style
-      if current_user.is_a?(GuestUser) 
-       (link_to "Register", new_user_registration_path, class: style) +
-       " ".html_safe +
-       (link_to "Login", new_user_session_path, class: style) 
-     else 
-       (link_to "Logout", destroy_user_session_path, method: :delete, class: style) +
+  def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
+    else
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: style) +
        " ".html_safe +
        (link_to "Edit Profile", edit_user_registration_path, class: style)
-      end 
+    end
   end
-  
-  def source_helper (layout_name)
-     if session[:source]
-       greeting =  "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
-     end 
+    end
   end
-  
+
   def copyright_generator
     GooseCopyrightTool::Renderer.copyright 'Kaden Ficklin', 'All rights reserved'
   end
