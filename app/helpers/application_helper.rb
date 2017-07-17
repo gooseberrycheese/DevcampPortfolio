2efb0dd6ad39c018e2481af319023aab8c61fad6
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 05:40:23 +0000

    implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,7 @@ module ApplicationHelper
       " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
-      (link_to "Logout", destroy_user_session_path, method: :delete, class: style) +
-       " ".html_safe +
-       (link_to "Edit Profile", edit_user_registration_path, class: style)
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
@@ -21,4 +19,44 @@ module ApplicationHelper
   def copyright_generator
     GooseCopyrightTool::Renderer.copyright 'Kaden Ficklin', 'All rights reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
